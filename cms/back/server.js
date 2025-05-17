07ad5aa1bfa89f8344568b96f1201e020274f443
Alwyn974 <alwyn974@gmail.com>
2025-05-17 16:27:42 +0400

    feat(cms): add public-facing index.html with assets and resizable dependency

--- cms/back/server.js
+++ cms/back/server.js
@@ -6,28 +6,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
 const static_1 = __importDefault(require("@fastify/static"));
 const node_path_1 = __importDefault(require("node:path"));
-const server = (0, fastify_1.default)({ logger: true });
+const server = (0, fastify_1.default)({ logger: true, });
+const prefix = '/editor';
 server.register(static_1.default, {
     root: node_path_1.default.join(__dirname, 'public')
 });
-server.get('/', (request, reply) => {
+server.get(prefix + '/', (request, reply) => {
     reply.send({ hello: 'world' });
 });
-if (typeof (PhusionPassenger) !== 'undefined') {
-    server.listen({ path: "passenger", host: "127.0.0.1" }, (err, address) => {
-        if (err) {
-            server.log.error(err);
-            process.exit(1);
-        }
-        console.log(`Server listening on ${address}`);
-    });
-}
-else {
-    server.listen({ port: 3000 }, (err, address) => {
-        if (err) {
-            server.log.error(err);
-            process.exit(1);
-        }
-        console.log(`Server listening on ${address}`);
-    });
-}
+const callback = (err, address) => {
+    if (err) {
+        server.log.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening on ${address}`);
+};
+if (typeof (PhusionPassenger) !== 'undefined')
+    server.listen({ path: "passenger", host: "127.0.0.1" }, callback);
+else
+    server.listen({ port: 3000 }, callback);
--- cms/back/server.ts
+++ cms/back/server.ts
@@ -3,31 +3,27 @@ import fastifyStatic from "@fastify/static"
 import path from "node:path"
 
 declare const PhusionPassenger: any
-const server = fastify({ logger: true })
+const server = fastify({logger: true,})
+const prefix = '/editor'
 
 server.register(fastifyStatic, {
     root: path.join(__dirname, 'public')
 })
 
-server.get('/', (request, reply) => {
+server.get(prefix + '/', (request, reply) => {
     reply.send({hello: 'world'})
 })
 
-if (typeof(PhusionPassenger) !== 'undefined') {
-    server.listen({ path: "passenger", host: "127.0.0.1"}, (err, address) => {
-        if (err) {
-            server.log.error(err)
-            process.exit(1)
-        }
-        console.log(`Server listening on ${address}`)
-    })
-} else {
-    server.listen({port: 3000}, (err, address) => {
-        if (err) {
-            server.log.error(err)
-            process.exit(1)
-        }
-        console.log(`Server listening on ${address}`)
-    })
+const callback: (err: Error | null, address: string) => void = (err, address) => {
+    if (err) {
+        server.log.error(err)
+        process.exit(1)
+    }
+    console.log(`Server listening on ${address}`)
 }
+
+if (typeof (PhusionPassenger) !== 'undefined')
+    server.listen({path: "passenger", host: "127.0.0.1"}, callback)
+else
+    server.listen({port: 3000}, callback)
 

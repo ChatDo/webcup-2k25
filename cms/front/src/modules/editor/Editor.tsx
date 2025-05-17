6e3af497efabb0b81f2827e7af6e01ba4ec225d1
Hugo BARET <hugo.baret@epitech.eu>
2025-05-17 15:56:28 +0400

    feat: add Editor and EditorLayout components with resizable functionality

--- cms/front/src/modules/editor/Editor.tsx
+++ cms/front/src/modules/editor/Editor.tsx
@@ -1,13 +1,11 @@
-import type {Component} from 'solid-js';
+import {Component} from "solid-js";
 import PageBuilder from "~/modules/editor/PageBuilder";
 import {EditorLayout} from "~/modules/editor/EditorLayout";
 
-const App: Component = () => {
+export const Editor: Component = () => {
     return (
         <EditorLayout>
             <PageBuilder/>
         </EditorLayout>
-    );
-};
-
-export default App;
+    )
+}

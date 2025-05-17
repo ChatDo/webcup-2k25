e7fd73ebb0fceada3a20316e900e82626a476156
Alwyn974 <alwyn974@gmail.com>
2025-05-17 15:07:19 +0400

    feat(cms-back): add TypeScript setup with dependencies and initial configuration

---
+++ cms/back/server.ts
@@ -0,0 +1,23 @@
+import fastify from "fastify"
+import fastifyStatic from "@fastify/static"
+import path from "node:path"
+
+const server = fastify()
+
+server.register(fastifyStatic, {
+    root: path.join(__dirname, 'public')
+})
+
+// Declare a route
+server.get('/', (request, reply) => {
+    reply.send({hello: 'world'})
+})
+
+// Run the server!
+server.listen({port: 3000}, (err, address) => {
+    if (err) {
+        server.log.error(err)
+        process.exit(1)
+    }
+    // Server is now listening on ${address}
+})

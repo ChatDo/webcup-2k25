0cb76c6df73e93c25b34f6c15f0b067dcb6bb422
Nicolas JULIE <nicolas.julie@epitech.eu>
2025-05-17 14:51:59 +0200

    Removed button, added href

--- landing/src/components/HelloWorld.vue
+++ landing/src/components/HelloWorld.vue
@@ -14,9 +14,6 @@
             <Bars3Icon class="size-6" aria-hidden="true" />
           </button>
         </div>
-        <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" class="text-sm/6 font-semibold text-gray-900">Se connecter <span aria-hidden="true">&rarr;</span></a>
-        </div>
       </nav>
       <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
         <div class="fixed inset-0 z-50" />
@@ -56,7 +53,7 @@
           <h1 class="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">En prévision de mon départ...</h1>
           <p class="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">ceci sera mon dernier mot.</p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
-            <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 font-semibold text-white shadow-sm hover:bg-indigo-500
+            <a href="editor" class="rounded-md bg-indigo-600 px-3.5 py-2.5 font-semibold text-white shadow-sm hover:bg-indigo-500
             focus-visible:outline focus-visible:outline-offset-2 text-xl
             focus-visible:outline-indigo-600">Je suis prêt</a>
           </div>
